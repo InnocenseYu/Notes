1f3591e1797a8e0d54b18a25833f001128f38694
InnocenseYu <shaun.xy.li@gmail.com>
2022-05-06 15:07:12 +0800

    C language learning, do while loop

--- C_CPP/C/StatementStructure/while&for&do_while.c
+++ C_CPP/C/StatementStructure/while&for&do_while.c
@@ -42,3 +42,54 @@ int main()
 
     return 0;
 }
+
+///////////////////////
+//计算n的阶乘////////
+/////////////////////
+
+int main()
+{
+    int k = 1;
+    int n = 0;
+    printf("please input n:");
+    scanf("%d",&n);
+    int i = 0;
+    for(i=1 ; i<=n; i++)
+    {
+        k = k*i;
+    }
+    printf("n! = %d\n",k);
+
+    return 0;
+}
+
+/////////////////////
+// 1+2!+3!+...+10!///
+/////////////////////
+int main()
+{
+    
+    int n = 0;
+    int sum = 0;
+    printf("please input n:");
+    scanf("%d", &n);
+
+    int j = 0;
+    for (j = 1; j <= n; j++)
+    {
+        int k = 1;
+        int i = 0;
+        for (i = 1; i <= j; i++)
+        {
+            k = k * i; //每个j值的阶乘，注意k变量要在作用域内初始化。
+                       //否则，当在j循环之外的话，每次i值变化，k的值将不会被重置为1，将保留上次使用的值
+            //阶乘计算，i<=j
+        }
+
+        sum += k;     
+
+    }
+    printf("sum = %d\n", sum);
+
+    return 0;
+}
