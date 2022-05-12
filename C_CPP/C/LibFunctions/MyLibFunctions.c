80bd3e6e9533878f5597be3d81ce087753c24d51
InnocenseYu <shaun.xy.li@gmail.com>
2022-05-12 17:40:01 +0800

    C language learning, strcpy function optimize

--- C_CPP/C/LibFunctions/MyLibFunctions.c
+++ C_CPP/C/LibFunctions/MyLibFunctions.c
@@ -1,11 +1,19 @@
-//#include<stdio.h>
-//#include <assert.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+#include <Windows.h>
+#include <string.h>
 
-int strlen(const char* str)
+///////////////////////////
+/////字符串长度函数优化//////
+///////////////////////////
+
+size_t strlen(const char* str); //size_t 是typedef 重命名的类型 unsigned int
 // 方法1
 int my_strlen(const char* str)
 {
-	assert(str != NULL)
+	assert(str != NULL);
 	
 	char* start = str;
 	char* end = str;
@@ -18,103 +26,110 @@ int my_strlen(const char* str)
 }
 
 //方法二
-// int my_strlen(char* str) 
-// {
-//    char* end = str;
-//    for( ; *end != '\0'; ++end)  //注意此for循环的写法，初值、操作内容没写也要用;表示
-//       ;
-//    return end - str;
-// }
+int my_strlen(char* str) 
+{
+   char* end = str;
+   for( ; *end != '\0'; ++end)  //注意此for循环的写法，初值、操作内容没写也要用;表示
+      ;
+   return end - str;
+}
 
 //方法三
-// int my_strlen(const char* str）
-// {
-// 	assert(str != NULL);
+int my_strlen(const char* str)
+{
+	assert(str != NULL);
 	
-// 	int count = 0;
-// 	while (*str)
-// 	{
-// 		count++;
-// 		str++;
-// 	}
+	int count = 0;
+	while (*str)
+	{
+		count++;
+		str++;
+	}
 
-// 	return count;
-// }
+	return count;
+}
 
 
-char* strcpy(char* destination, const char* source)
+///////////////////////////////
+/////字符串内容复制函数优化//////
+///////////////////////////////
+
 // strcpy 系统库函数，需包含 #include <string.h>
 //字符串拷贝
-//原代码
-// void my_strcpy(char* dest, char* src)
-// {
-// 	while(*src != '\0')
-// 	{
-// 		*dest = *src;
-// 		src++;
-// 		dest++;
-// 	}
-// 	*dest = *src; //copy '\0'
-// } 
+char* strcpy(char* destination, const char* source);
 
-    //代码优化1
-//void my_strcpy(char* dest, char* src)
-//{
-	// while(*src != '\0')	  
-	// {
-	// 	*dest++ = *src++;
-	// }
-	// *dest = *src;
-//}
+//优化前代码
+void my_strcpy(char* dest, char* src)
+{
+	while(*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = *src; //copy '\0'
+} 
 
-
-	//代码优化2
-//void my_strcpy(char* dest, char* src)
-//{
-	// while(*dest++ = *src++)  //while判断条件为：*src赋值到*dest后的内容是否为真，真的话执行{}
-	// {
-	// 	;
-	// }
-//}
+//代码优化1
+void my_strcpy(char* dest, char* src)
+{
+	while(*src != '\0')	  
+	{
+		*dest++ = *src++;
+	}
+	*dest = *src;
+}
 
 
-	//代码优化3   //当是NULL时，程序不进行赋值拷贝，正常执行，不至于程序崩溃产生影响
-//void my_strcpy(char* dest, char* src)
-//{
-	// if(src != NULL & dest != NULL)
-	// while (*dest++ = *src++)
-	// {
-	// 	;
-	// }
-//}	
+//代码优化2
+void my_strcpy(char* dest, char* src)
+{
+	while(*dest++ = *src++)  //while判断条件为：*src赋值到*dest后的内容是否为真，真的话执行{}, 
+	                         //字符串*src最后一次将未显示的字符'\0'赋值给*dest,此时*dest值非真，跳出循环
+							 //可以完成'\0'的赋值
+	{
+		;
+	}
+}
 
-	//代码优化4    //assert() 断言函数的使用，
-// void my_strcpy(char* dest, char* src)
-// {
-// 	assert(src != NULL);//断言，必须引用头文件 #include <assert.h>
-// 	assert(dest != NULL);
 
-// 	while (*dest++ = *src++)
-// 	{
-// 	 	;
-// 	}
-// }
+//代码优化3   //当是NULL时，程序不进行赋值拷贝，正常执行，不至于程序崩溃产生影响
+void my_strcpy(char* dest, char* src)
+{
+	if(src != NULL & dest != NULL) //逻辑与 还是 位与
+	while (*dest++ = *src++)
+	{
+		;
+	}
+}	
+
+//代码优化4    //assert() 断言函数的使用，
+void my_strcpy(char* dest, char* src)
+{
+	assert(src != NULL);//断言，必须引用头文件 #include <assert.h>
+	assert(dest != NULL);
+
+	while (*dest++ = *src++)
+	{
+	 	;
+	}
+}
  
 
 //代码优化5  //const 修饰*src，防止src、dest两个地址前后位置调换出现错误
-// void my_strcpy(char* dest, const char* src)
-// {
-// 	assert(src != NULL);//断言，必须引用头文件 #include <assert.h>
-// 	assert(dest != NULL);
+void my_strcpy(char* dest, const char* src)
+{
+	assert(src != NULL);//断言，必须引用头文件 #include <assert.h>
+	assert(dest != NULL);
 
-// 	while (*dest++ = *src++)
-// 	{
-// 	 	;
-// 	}
-// }
+	while (*dest++ = *src++)
+	{
+	 	;
+	}
+}
 
 
-//代码优化6  //修改void 为 char* ,函数内部return 函数地址，给定函数返回值，可以直接作为参数被其他函数调用使用，简称：链式访问
+//代码优化6  //修改void 为 char* ,给定函数返回值，可以直接作为参数被其他函数调用使用，简称：链式访问，函数内部 return 操作
 char* my_strcpy(char* dest, const char* src) //const 修饰*src，防止src、dest两个地址因前后位置调换*src被操作，进而出现错误
 {
 	char* ret = dest;     //数组起始地址保存
